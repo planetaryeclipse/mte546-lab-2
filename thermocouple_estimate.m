e9da798935b3f09bbe62e9227ca19c33c9b63ad9
Samuel Street <planetaryeclipse@gmail.com>
2025-02-12 19:06:49 -0500

    Created inverse model for distance from thermocouple temperature

---
+++ thermocouple_estimate.m
@@ -0,0 +1,14 @@
+function [mu, sigma] = thermocouple_estimate(props,tl_temp,tr_temp,br_temp,bl_temp)
+%THERMOCOUPLE_ESTIMATE Estimate the location and covariance of the Peltier
+%heater on the plate
+
+% combination of tl,tr,br
+
+% combination of tr,br,bl
+
+% combination of br,bl,tl
+
+% combination of bl,tl,tr
+
+end
+
